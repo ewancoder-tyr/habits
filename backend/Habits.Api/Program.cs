14c9465eb1ea13665cf56590f63610147b320b74
Ivan Zyranau <ewancoder@gmail.com>
2025-02-16 02:08:09 +0400

    Add logging & improve saving only when needed

--- backend/Habits.Api/Program.cs
+++ backend/Habits.Api/Program.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Routing.Constraints;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Serilog;
+using Serilog.Events;
 
 var GoogleClient = "725292928539-ebtufhfemopng7t4akjd9tpatun9fkgd.apps.googleusercontent.com";
 var builder = WebApplication.CreateSlimBuilder(args);
@@ -69,6 +71,20 @@ builder.Services.AddAuthentication()
     });
 builder.Services.AddAuthorization();
 
+builder.Host.UseSerilog((context, config) =>
+{
+    config
+        .MinimumLevel.Information()
+        .MinimumLevel.Override("Habits", LogEventLevel.Verbose)
+        .WriteTo.Console();
+
+#if !DEBUG
+    config.WriteTo.Seq(
+        builder.Configuration["SeqUri"] ?? throw new InvalidOperationException("Cannot read seq uri secret."),
+        apiKey: builder.Configuration["SeqApiKey"]);
+#endif
+});
+
 var app = builder.Build();
 
 app.UseSwagger();
@@ -78,28 +94,48 @@ app.UseCors(builder => builder
     .AllowAnyMethod()
     .WithHeaders("Authorization", "Content-Type"));
 
+var logger = app.Services.GetRequiredService<ILogger<HabitsApp>>();
+logger.LogInformation("Starting the application");
+
+var needToSave = false;
 var db = new Dictionary<string, List<Habit>>();
 if (!Directory.Exists("data"))
     Directory.CreateDirectory("data");
 if (File.Exists("data/db"))
 {
+    logger.LogInformation("Found database file, loading the data.");
     var content = await File.ReadAllTextAsync("data/db");
-    db = JsonSerializer.Deserialize(content, SerializerContext.Default.DictionaryStringListHabit);
-    if (db is null) throw new InvalidOperationException();
+    try
+    {
+        db = JsonSerializer.Deserialize(content, SerializerContext.Default.DictionaryStringListHabit);
+        if (db is null) throw new InvalidOperationException("Could not deserialize the database");
+    }
+    catch (Exception exception)
+    {
+        logger.LogError(exception, "Could not load the database");
+        throw;
+    }
 }
 
 var saver = Task.Run(async () =>
 {
     while (true)
     {
-        try
+        if (needToSave)
         {
-            var serialized = JsonSerializer.Serialize(db, SerializerContext.Default.DictionaryStringListHabit);
-            await File.WriteAllTextAsync("data/db", serialized);
-        }
-        catch (Exception exception)
-        {
-            Console.WriteLine(exception);
+            try
+            {
+                logger.LogInformation("Changes were detected, saving the data");
+
+                var serialized = JsonSerializer.Serialize(db, SerializerContext.Default.DictionaryStringListHabit);
+                await File.WriteAllTextAsync("data/db", serialized);
+                needToSave = false;
+                logger.LogInformation("Successfully saved the data");
+            }
+            catch (Exception exception)
+            {
+                logger.LogError(exception, "Failed to save the data");
+            }
         }
 
         await Task.Delay(TimeSpan.FromMinutes(1));
@@ -154,6 +190,7 @@ habitsGroup.MapPost("/", Results<NotFound, BadRequest<string>, Created<Created>>
     };
 
     habits.Add(habit);
+    needToSave = true;
     return TypedResults.Created("/api/habits", new Created(habit.Name));
 });
 habitsGroup.MapPut("/{id}", Results<NotFound, BadRequest<string>, Ok<Habit>> (string id, CreateHabit body, ClaimsPrincipal user) =>
@@ -173,6 +210,7 @@ habitsGroup.MapPut("/{id}", Results<NotFound, BadRequest<string>, Ok<Habit>> (st
 
     habit.Name = body.Name;
     habit.LengthDays = body.LengthDays;
+    needToSave = true;
     return TypedResults.Ok(habit);
 });
 habitsGroup.MapPost("/{id}/days/{day}", Results<NotFound, Ok<Habit>> (string id, int day, ClaimsPrincipal user) =>
@@ -188,6 +226,7 @@ habitsGroup.MapPost("/{id}/days/{day}", Results<NotFound, Ok<Habit>> (string id,
         return TypedResults.NotFound();
 
     habit.Days.Add(day);
+    needToSave = true;
     return TypedResults.Ok(habit);
 });
 habitsGroup.MapDelete("/{id}/days/{day}", Results<NotFound, Ok<Habit>> (string id, int day, ClaimsPrincipal user) =>
@@ -203,6 +242,7 @@ habitsGroup.MapDelete("/{id}/days/{day}", Results<NotFound, Ok<Habit>> (string i
         return TypedResults.NotFound();
 
     habit.Days.Remove(day);
+    needToSave = true;
     return TypedResults.Ok(habit);
 });
 
@@ -232,3 +272,8 @@ internal partial class SerializerContext : JsonSerializerContext
 }
 
 internal sealed record Created(string Id);
+
+/// <summary>
+/// A stub class for logging context.
+/// </summary>
+internal sealed record HabitsApp();
